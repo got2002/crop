d96156950b15c1a9851637d092a24a240d2d6cb7
got2002 <gohvfjb@gmail.com>
2023-11-08 15:08:33 +0700

    v2

--- process_imageV2.py
+++ process_imageV2.py
@@ -58,8 +58,7 @@ def crop_and_save_images_from_db(input_cases,input_bullet,output_dir):
 
         
         for i, row in enumerate(rows):
-            #image_path = input_bullet +  f"{row[1]}.jpeg"
-            image_path= os.path.join(input_cases, f"{row[1]}.jpeg")
+            image_path = os.path.join(input_cases, f"{row[1]}.jpeg")
             img = Image.open(image_path)
             width, height = img.size
 
@@ -70,13 +69,14 @@ def crop_and_save_images_from_db(input_cases,input_bullet,output_dir):
                 crop_coords = tuple(int(val * width if idx % 2 == 0 else val * height) for idx, val in enumerate(crop_coords))
                 cropped_img = img.crop(crop_coords)
                 output_image_path = os.path.join(output_dir, f"{row[0]}_{j + 1}.jpg")
-                
+
                 if not cropped_img.getbbox():
-                    print(f"Skipping empty crop: {output_image_path}")
+                    error_message = f"Image not saved due to empty crop: {output_image_path}"
+                    logging.info(error_message)
                 else:
                     cropped_img.save(output_image_path)
-                   # print(f"Cropped image {i + 1}_{j + 1} saved to {output_image_path}")
-
+                    success_message = f"Cropped image {i + 1}_{j + 1} saved to {output_image_path}"
+                    logging.info(success_message)
         # cases finish
 
         
@@ -87,24 +87,25 @@ def crop_and_save_images_from_db(input_cases,input_bullet,output_dir):
         rows = cursor.fetchall()
         
         for i, row in enumerate(rows):
-            image_path = row[1] + ".jpeg"
-            image_path= os.path.join(input_bullet, f"{row[1]}.jpeg")
+            image_path = os.path.join(input_bullet, f"{row[1]}.jpeg")
             img = Image.open(image_path)
             width, height = img.size
 
             os.makedirs(output_dir, exist_ok=True)
 
-            # Crop and save each section for folder 2 Bullet 
+            # Crop and save each section for folder 2 Bullet
             for j, crop_coords in enumerate(scale_crop_bullet):
                 crop_coords = tuple(int(val * width if idx % 2 == 0 else val * height) for idx, val in enumerate(crop_coords))
                 cropped_img = img.crop(crop_coords).rotate(90, expand=True)
                 output_image_path = os.path.join(output_dir, f"{row[0]}_{j + 1}.jpg")
+
                 if not cropped_img.getbbox():
-                    print(f"Skipping empty crop: {output_image_path}")
+                    error_message = f"Image not saved due to empty crop: {output_image_path}"
+                    logging.info(error_message)
                 else:
                     cropped_img.save(output_image_path)
-                   # print(f"Cropped image {i + 1}_{j + 1} saved to {output_image_path}")
-
+                    success_message = f"Cropped image {i + 1}_{j + 1} saved to {output_image_path}"
+                    logging.info(success_message)
         #bullet finish            
         conn.close()
     except Exception as e:
